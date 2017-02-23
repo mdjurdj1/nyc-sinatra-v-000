e79743fae9266dab550c67a609ae42a471640e9b
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-02-23 19:31:53 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,6 @@
 class FiguresController < Sinatra::Base
-  
+enable :sessions
+use Rack::Flash
 set :views, Proc.new { File.join(root, "../views/") }
 register Sinatra::Twitter::Bootstrap::Assets
 
@@ -14,18 +15,31 @@ register Sinatra::Twitter::Bootstrap::Assets
 
   post '/figures' do 
     @figure = Figure.new(params[:figure])
-    if !params[:landmark][:name].empty? 
-      @figure.landmarks << Landmark.create(name: params[:landmark][:name])
-    elsif !params[:title][:name].empty?
-      @figure.titles << Title.create(name: params[:title][:name])
-    end 
+    @figure.landmarks << Landmark.create(name: params[:landmark][:name]) if !params[:landmark][:name].empty? 
+    @figure.titles << Title.create(name: params[:title][:name]) if !params[:title][:name].empty?
     @figure.save 
+    flash[:message] = "Successfully created figure."
     erb :'/figures/show'
   end 
 
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
     erb :'/figures/show'
+  end 
+
+  get '/figures/:id/edit' do 
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end 
+
+  patch '/figures/:id' do 
+    @figure = Figure.find(params[:id])
+    @figure.update(name: params[:figure][:name]) if !params[:figure][:name].empty?
+    @figure.landmarks << Landmark.create(name: params[:landmark][:name])  if !params[:landmark][:name].empty? 
+    @figure.titles << Title.create(name: params[:title][:name])  if !params[:title][:name].empty?
+    @figure.save
+    flash[:message] = "Successfully updated song."
+    erb :'figures/show'
   end 
   
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,23 @@
 class LandmarksController < ApplicationController
+enable :sessions
+use Rack::Flash
+set :views, Proc.new { File.join(root, "../views/") }
+register Sinatra::Twitter::Bootstrap::Assets
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.new(params[:landmark])
+    @landmark.save
+    erb :'landmarks/show'
+  end
+
 
 end
