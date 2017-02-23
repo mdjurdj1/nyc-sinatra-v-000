16c3decf94d99ac20932da2ffd306426707ebd30
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-02-23 19:43:00 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -19,5 +19,21 @@ register Sinatra::Twitter::Bootstrap::Assets
     erb :'landmarks/show'
   end
 
+  get '/landmarks/:id' do
+   @landmark = Landmark.find(params[:id])
+   erb :'landmarks/show'
+ end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(name: params[:landmark][:name]) if !params[:landmark][:name].empty?
+    @landmark.update(year_completed: params[:landmark][:year_completed]) if !params[:landmark][:year_completed].empty?
+    erb :'landmarks/show'
+  end
 
 end
